d41a7c6e11f7c218c5479e9700ac984a3e61291a
Fernando Ezequiel Mancuso <fmancuso@makingsense.com>
2021-04-16 15:03:23 -0300

    Adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
 import LoginTab from './loginForm.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          Herd
-      </header>
-      <div className="LogIn">
-        <LoginTab/>
-      </div>
+      <Container className="p-3">
+        <Jumbotron>
+          <header className="App-header">
+              Herd
+          </header>
+        </Jumbotron>
+        <div className="LogIn">
+          <LoginTab/>
+        </div>
+      </Container>
     </div>
   );
 }
